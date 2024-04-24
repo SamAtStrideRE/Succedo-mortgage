e3ee6a68a189831076e4ff2c884e859839ffb82f
Barnabas-techWorks <barneytechworks@gmail.com>
2024-04-25 02:13:53 +0530

    navbar solved

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import {
   FiAlignRight,
-  // FiXCircle,
   FiChevronDown,
   FiChevronUp,
 } from "react-icons/fi";
@@ -12,30 +11,26 @@ import "./header.css";
 const Header = () => {
   const [isMenu, setisMenu] = useState(false);
   const [isResponsiveclose, setResponsiveclose] = useState(false);
+  
   const toggleClass = () => {
-    setisMenu(isMenu === false ? true : false);
-    setResponsiveclose(isResponsiveclose === false ? true : false);
+    setisMenu(!isMenu);
+    setResponsiveclose(!isResponsiveclose);
   };
-  let boxClass = ["main-menu menu-right menuq1"];
-  if (isMenu) {
-    boxClass.push("menuq2");
-  } else {
-    boxClass.push("");
-  }
-  const [isMenuSubMenu, setMenuSubMenu] = useState(false);
-  const toggleSubmenu = () => {
-    setMenuSubMenu(isMenuSubMenu === false ? true : false);
-  };
-  let boxClassSubMenu = ["sub__menus"];
-  if (isMenuSubMenu) {
-    boxClassSubMenu.push("sub__menus__Active");
-  } else {
-    boxClassSubMenu.push("");
-  }
 
-  function setMenuHandler() {
-    setisMenu((currState) => !currState);
-  }
+  const [isMenuSubMenu1, setMenuSubMenu1] = useState(false);
+  const toggleSubmenu1 = () => {
+    setMenuSubMenu1(!isMenuSubMenu1);
+  };
+
+  const [isMenuSubMenu2, setMenuSubMenu2] = useState(false);
+  const toggleSubmenu2 = () => {
+    setMenuSubMenu2(!isMenuSubMenu2);
+  };
+
+  const [isMenuSubMenu3, setMenuSubMenu3] = useState(false);
+  const toggleSubmenu3 = () => {
+    setMenuSubMenu3(!isMenuSubMenu3);
+  };
 
   return (
     <header className="header__middle">
@@ -46,178 +41,104 @@ const Header = () => {
         </NavLink>
       </div>
 
-      <nav className="main-nav ">
+      <nav className="main-nav">
         {/* Responsive Menu Button */}
-
-        {/* <span className="menubar__button">
-          <FiXCircle />
-        </span> */}
-
-        <span className="menubar__button">
+        <span className="menubar__button" onClick={toggleClass}>
           <FiAlignRight />
         </span>
 
-        <ul className={boxClass.join(" ")}>
+        <ul className={`main-menu ${isMenu ? "menuq2" : ""}`}>
           <li className="menu-item">
-            <NavLink
-              exact
-              activeClassName="is-active"
-              onClick={toggleClass}
-              to={`/`}
-            >
-              {" "}
-              Home{" "}
+            <NavLink exact activeClassName="is-active" to="/">
+              Home
             </NavLink>
           </li>
-          <li className="menu-item ">
-            <NavLink
-              onClick={toggleClass}
-              activeClassName="is-active"
-              to={`/About`}
-            >
-              {" "}
-              About Us{" "}
-            </NavLink>{" "}
+          <li className="menu-item">
+            <NavLink activeClassName="is-active" to="/About">
+              About Us
+            </NavLink>
           </li>
-          <li onClick={toggleSubmenu} className="menu-item sub__menus__arrows">
-            {" "}
-            <Link to="#" onClick={setMenuHandler}>
-              {" "}
-              Mortgage Products {isMenu ? <FiChevronDown /> : <FiChevronUp />}{" "}
+          <li className="menu-item sub__menus__arrows">
+            <Link to="#" onClick={toggleSubmenu1}>
+              Mortgage Products {isMenuSubMenu1 ? <FiChevronUp /> : <FiChevronDown />}
             </Link>
-            {isMenu && (
-              <ul className={boxClassSubMenu.join(" ")}>
+            {isMenuSubMenu1 && (
+              <ul className="sub__menus">
                 <li>
-                  {" "}
-                  <NavLink
-                    onClick={toggleClass}
-                    activeClassName="is-active"
-                    to={`/Online`}
-                  >
-                    {" "}
-                    Fixed-rate Mortgages{" "}
-                  </NavLink>{" "}
+                  <NavLink activeClassName="is-active" to="/Online">
+                    Fixed-rate Mortgages
+                  </NavLink>
                 </li>
                 <li>
-                  <NavLink
-                    onClick={toggleClass}
-                    activeClassName="is-active"
-                    to={`/Offline`}
-                  >
-                    {" "}
-                    FHA Loans{" "}
-                  </NavLink>{" "}
+                  <NavLink activeClassName="is-active" to="/Offline">
+                    FHA Loans
+                  </NavLink>
                 </li>
                 <li>
-                  <NavLink
-                    onClick={toggleClass}
-                    activeClassName="is-active"
-                    to={`/Offline`}
-                  >
-                    {" "}
-                    VA Loans{" "}
-                  </NavLink>{" "}
+                  <NavLink activeClassName="is-active" to="/Offline">
+                    VA Loans
+                  </NavLink>
                 </li>
               </ul>
             )}
           </li>
-          <li onClick={toggleSubmenu} className="menu-item sub__menus__arrows">
-          {" "}
-          <Link to="#" onClick={setMenuHandler}>
-            {" "}
-            Rates {isMenu ? <FiChevronDown /> : <FiChevronUp />}{" "}
-          </Link>
-          {isMenu && (
-            <ul className={boxClassSubMenu.join(" ")}>
-              <li>
-                {" "}
-                <NavLink
-                  onClick={toggleClass}
-                  activeClassName="is-active"
-                  to={`/Online`}
-                >
-                  {" "}
-                  Current rates{" "}
-                </NavLink>{" "}
-              </li>
-              <li>
-                <NavLink
-                  onClick={toggleClass}
-                  activeClassName="is-active"
-                  to={`/Offline`}
-                >
-                  {" "}
-                  Factors Affecting Rates{" "}
-                </NavLink>{" "}
-              </li>
-              <li>
-                <NavLink
-                  onClick={toggleClass}
-                  activeClassName="is-active"
-                  to={`/Offline`}
-                >
-                  {" "}
-                  Rate Trends{" "}
-                </NavLink>{" "}
-              </li>
-            </ul>
-          )}
-        </li>
-          <li onClick={toggleSubmenu} className="menu-item sub__menus__arrows">
-          {" "}
-          <Link to="#" onClick={setMenuHandler}>
-            {" "}
-            Apply Here {isMenu ? <FiChevronDown /> : <FiChevronUp />}{" "}
-          </Link>
-          {isMenu && (
-            <ul className={boxClassSubMenu.join(" ")}>
-              <li>
-                {" "}
-                <NavLink
-                  onClick={toggleClass}
-                  activeClassName="is-active"
-                  to={`/Online`}
-                >
-                  {" "}
-                  Online Application{" "}
-                </NavLink>{" "}
-              </li>
-              <li>
-                <NavLink
-                  onClick={toggleClass}
-                  activeClassName="is-active"
-                  to={`/Offline`}
-                >
-                  {" "}
-                  Document Checklist{" "}
-                </NavLink>{" "}
-              </li>
-              <li>
-                <NavLink
-                  onClick={toggleClass}
-                  activeClassName="is-active"
-                  to={`/Offline`}
-                >
-                  {" "}
-                  Application Process{" "}
-                </NavLink>{" "}
-              </li>
-            </ul>
-          )}
-        </li>
-          <li className="menu-item ">
-            <NavLink
-              onClick={toggleClass}
-              activeClassName="is-active"
-              to={`/Contact`}
-            >
-              {" "}
-              Contact{" "}
-            </NavLink>{" "}
+          <li className="menu-item sub__menus__arrows">
+            <Link to="#" onClick={toggleSubmenu2}>
+              Rates {isMenuSubMenu2 ? <FiChevronUp /> : <FiChevronDown />}
+            </Link>
+            {isMenuSubMenu2 && (
+              <ul className="sub__menus">
+                <li>
+                  <NavLink activeClassName="is-active" to="/Online">
+                    Current rates
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink activeClassName="is-active" to="/Offline">
+                    Factors Affecting Rates
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink activeClassName="is-active" to="/Offline">
+                    Rate Trends
+                  </NavLink>
+                </li>
+              </ul>
+            )}
+          </li>
+          <li className="menu-item sub__menus__arrows">
+            <Link to="#" onClick={toggleSubmenu3}>
+              Apply Here {isMenuSubMenu3 ? <FiChevronUp /> : <FiChevronDown />}
+            </Link>
+            {isMenuSubMenu3 && (
+              <ul className="sub__menus">
+                <li>
+                  <NavLink activeClassName="is-active" to="/Online">
+                    Online Application
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink activeClassName="is-active" to="/Offline">
+                    Document Checklist
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink activeClassName="is-active" to="/Offline">
+                    Application Process
+                  </NavLink>
+                </li>
+              </ul>
+            )}
+          </li>
+          <li className="menu-item">
+            <NavLink activeClassName="is-active" to="/Contact">
+              Contact
+            </NavLink>
           </li>
         </ul>
       </nav>
     </header>
   );
 };
+
 export default Header;
