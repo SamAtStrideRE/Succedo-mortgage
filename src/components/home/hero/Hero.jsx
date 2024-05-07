2b0e56024f5be22df364e57f0c43dd4cefaa9404
TechSammy <mrsam03102002@gmail.com>
2024-05-08 01:11:02 +0530

    changed the positions in navbaar

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -9,27 +9,6 @@ const Hero = () => {
         <div className='container'>
           <Heading title='YOUR SUCCESS IS OUR PRIORITY'  />
           
-
-          {/* <form className='flex'>
-            <div className='box'>
-            <span>City/Street</span>
-            <input type='text' placeholder='Location' />
-            </div>
-            <div className='box'>
-            <span>Property Type</span>
-            <input type='text' placeholder='Property Type' />
-            </div>
-            <div className='box'>
-              <span>Price Range</span>
-              <input type='text' placeholder='Price Range' />
-              </div>
-            <div className='box'>
-              <h4>Advance Filter</h4>
-            </div>
-            <button className='btn1'>
-              <i className='fa fa-search'></i>
-              </button>
-            </form> */}
             
         </div>
       </section>
