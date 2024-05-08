9b8054484e8605d0018faba32531e34cde311498
TechSammy <mrsam03102002@gmail.com>
2024-05-08 22:55:14 +0530

    added buttons and texts

--- src/components/home/featured/FeaturedCard.jsx
+++ src/components/home/featured/FeaturedCard.jsx
@@ -1,20 +1,36 @@
-import React from "react"
-import { featured } from "../../data/Data"
+import React from "react";
+import { featured } from "../../data/Data";
+import "./Featured.css";
 
 const FeaturedCard = () => {
   return (
-    <>
-      <div className='content grid3 mtop'>
-        {featured.map((items, index) => (
-          <div className='box' key={index}>
-            <img src={items.cover} alt='' />
-            <h4>{items.name}</h4>
-            <label>{items.body}</label>
+    <div className='content grid3 mtop'>
+      {featured.map((item, index) => (
+        <div className='box' key={index}>
+          <img src={item.cover} alt='' />
+          <h4>{item.name}</h4>
+          <label>{item.body}</label>
+          <div className="benefits">
+            <p>Benefits</p>
+            <div className="benefits-list">
+              <div className="benefit-item">
+                <img src="path/to/your/icon1.png" alt="icon1" />
+                <span>Get Your Offer Accepted</span>
+              </div>
+              <div className="benefit-item">
+                <img src="path/to/your/icon2.png" alt="icon2" />
+                <span>Know Your Buying Range</span>
+              </div>
+            </div>
           </div>
-        ))}
-      </div>
-    </>
-  )
-}
+          <div className="buttons">
+            <button>Button 1</button>
+            <button>Button 2</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default FeaturedCard
+export default FeaturedCard;
