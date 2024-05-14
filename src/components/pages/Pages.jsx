55828ffb522721b7b550dd97db14edb4020af310
TechSammy <mrsam03102002@gmail.com>
2024-05-14 19:07:13 +0530

    navbar responsive done

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -6,8 +6,12 @@ import Footer from "../common/footer/Footer"
 import About from "../about/About"
 import Pricing from "../pricing/Pricing"
 import Blog from "../blog/Blog"
-import Services from "../services/Services"
+import FHA from "../mortgages/FHA"
 import Contact from "../contact/Contact"
+import ARM from "../mortgages/ARM"
+import FRM from "../mortgages/FRM"
+import JL from "../mortgages/JL"
+import VA from "../mortgages/VA"
 
 const Pages = () => {
   return (
@@ -17,7 +21,15 @@ const Pages = () => {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
-          <Route exact path='/services' component={Services} />
+
+          {/* Mortgages Pages */}
+          <Route exact path='/ARM' component={ARM} />
+          <Route exact path='/FHA' component={FHA} />
+          <Route exact path='/FRM' component={FRM} />
+          <Route exact path='/JL' component={JL} />
+          <Route exact path='/VA' component={VA} />
+
+
           <Route exact path='/blog' component={Blog} />
           <Route exact path='/pricing' component={Pricing} />
           <Route exact path='/contact' component={Contact} />
