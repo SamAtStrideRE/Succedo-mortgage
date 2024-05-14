3520cb7bda99583aa7a1848744be0637b4198a09
barneyStride <barnabas.johnson@stridere.com>
2024-05-14 19:17:27 +0530

    added mortgage files

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -8,6 +8,7 @@ import Pricing from "../pricing/Pricing"
 import Blog from "../blog/Blog"
 import Services from "../services/Services"
 import Contact from "../contact/Contact"
+import MortCalc from "../mortgageCalculator/MortgageCalculator"
 
 const Pages = () => {
   return (
@@ -21,6 +22,7 @@ const Pages = () => {
           <Route exact path='/blog' component={Blog} />
           <Route exact path='/pricing' component={Pricing} />
           <Route exact path='/contact' component={Contact} />
+          <Route exact path='/mortgagecalc' component={MortCalc} />
         </Switch>
         <Footer />
       </Router>
