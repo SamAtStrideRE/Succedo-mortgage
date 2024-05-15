5dddbdca40254d2ebc8f3bb385013f02f33df614
TechSammy <mrsam03102002@gmail.com>
2024-05-15 20:22:34 +0530

    ..

--- src/components/mortgageCalculator/calcComponents/Navbar.js
+++ src/components/mortgageCalculator/calcComponents/Navbar.js
@@ -1,17 +1,21 @@
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import { Container } from "@mui/system";
+// import AppBar from "@mui/material/AppBar";
+// import Toolbar from "@mui/material/Toolbar";
+// import Typography from "@mui/material/Typography";
+// import { Container } from "@mui/system";
+import Header from "../../common/header/Header.jsx";
+
 
 const Navbar = () => {
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar>
-          <Typography variant="h5">Bank of React</Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    // <AppBar position="static">
+    //   <Container maxWidth="xl">
+    //     <Toolbar>
+    //       <Typography variant="h5">Bank of React</Typography>
+    //     </Toolbar>
+    //   </Container>
+    // </AppBar>
+    <Header/>
+
   );
 };
 
