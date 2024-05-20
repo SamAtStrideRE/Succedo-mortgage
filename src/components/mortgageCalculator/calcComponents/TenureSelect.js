4b95ad37ebb4d26d56155fc2a417cb957ff2d22e
TechSammy <mrsam03102002@gmail.com>
2024-05-20 22:37:49 +0530

    mortgage years changed to 30

--- src/components/mortgageCalculator/calcComponents/TenureSelect.js
+++ src/components/mortgageCalculator/calcComponents/TenureSelect.js
@@ -17,14 +17,14 @@ const TenureSelect = ({ data, setData }) => {
         id="demo-simple-select"
         value={data.loanTerm}
         label="Tenure"
-        defaultValue={5}
+        defaultValue={10}
         onChange={handleChange}
       >
-        <MenuItem value={5}>5 years</MenuItem>
         <MenuItem value={10}>10 years</MenuItem>
         <MenuItem value={15}>15 years</MenuItem>
         <MenuItem value={20}>20 years</MenuItem>
         <MenuItem value={25}>25 years</MenuItem>
+        <MenuItem value={30}>30 years</MenuItem>
       </Select>
     </FormControl>
   );
