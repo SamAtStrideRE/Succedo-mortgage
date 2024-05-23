5ee3f2b20373dade799a32444e43d0a47ec0616d
TechSammy <mrsam03102002@gmail.com>
2024-05-24 01:16:04 +0530

    ...

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
             <div className='logo'>
               <img src='../images/logo-light.png' alt='' />
               <h2>Do You Need Help With Anything?</h2>
-              <p>Receive updates, hot deals, tutorials, discounts sent straignt in your inbox every month</p>
+              
 
               <div className='input flex'>
                 <input className="email" type='text' placeholder='Email Address' />
